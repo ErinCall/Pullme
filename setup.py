0147809b494d1a46c18afb7ecf1f60be49425a77
Andrew Lorente <andrew.lorente@gmail.com>
2011-12-30 21:47:31 -0800

    VERSION 2!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="pullme",
-      version="1.0.1",
+      version="2.0.0",
       description="A smart pull script for GitHub",
       author="Andrew Lorente",
       author_email="andrew.lorente@gmail.com",
